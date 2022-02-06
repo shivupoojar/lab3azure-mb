458865064b5076776b762d870aa582435803caff
Jakob Mass <jaks@ut.ee>
2022-02-06 21:14:49 +0200

    Template message board v1 with GET

--- app.py
+++ app.py
@@ -1,7 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+
+def read_messages_from_file():
+    with open('messages.txt') as messages_file:
+        return messages_file.readlines()
+
+
+def append_message_to_file(content):
+    with open('messages.txt', mode='a') as messages_file:
+        messages_file.write(content + "\n")
+
+
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def home():
+    new_message = request.args.get('msg')
+    if new_message:
+        append_message_to_file(new_message)
+
+    messages = read_messages_from_file()
+    return render_template('home.html', messages=messages)
