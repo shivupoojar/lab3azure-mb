f4e9a32a91b58a72691247c0fd7e44de86e48f33
jaks <jaks@ut.ee>
2022-02-07 14:58:39 +0200

    First version of JSON variant

--- app.py
+++ app.py
@@ -1,18 +1,30 @@
+import json
+from datetime import datetime
+
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
 def read_messages_from_file():
-    with open('messages.txt') as messages_file:
-        return messages_file.readlines()
+    """ Read all messages from a JSON file"""
+    with open('messages.json') as messages_file:
+        return json.load(messages_file)
 
 
 def append_message_to_file(content):
-    with open('messages.txt', mode='a') as messages_file:
-        messages_file.write(content + "\n")
+    """ Read the contents of JSON file, add this message to it's contents, write it to disk. """
+    messages = read_messages_from_file()
+    new_message = {
+        'content': content,
+        'timestamp': datetime.now().isoformat(" ", "seconds")
+    }
+    messages['messages'].append(new_message)
+    with open('messages.json', mode='w') as messages_file:
+        json.dump(messages, messages_file)
 
 
+# The Flask route, defining the main behaviour of the webserver:
 @app.route("/")
 def home():
     new_message = request.args.get('msg')
@@ -20,4 +32,4 @@ def home():
         append_message_to_file(new_message)
 
     messages = read_messages_from_file()
-    return render_template('home.html', messages=messages)
+    return render_template('home.html', messages=messages['messages'])
